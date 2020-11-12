dbc10a8915aba4e1931f676d669b31c9cae39cd2
yu <chu2205ssd@gmail.com>
2020-11-12 20:33:03 +0800

    加入專案檔案。

---
+++ WP_2020_01_HW02_A32000/frmGame.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WP_2020_01_HW02_A32000
+{
+    partial class frmGame
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.labLog = new System.Windows.Forms.Label();
+            this.picResult = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Font = new System.Drawing.Font("微軟正黑體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnDraw.Location = new System.Drawing.Point(577, 479);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(195, 70);
+            this.btnDraw.TabIndex = 0;
+            this.btnDraw.Text = "抽牌~~~";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.rtbLog.Location = new System.Drawing.Point(79, 85);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.Size = new System.Drawing.Size(263, 357);
+            this.rtbLog.TabIndex = 2;
+            this.rtbLog.Text = "";
+            // 
+            // labLog
+            // 
+            this.labLog.AutoSize = true;
+            this.labLog.Font = new System.Drawing.Font("微軟正黑體", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labLog.Location = new System.Drawing.Point(37, 28);
+            this.labLog.Name = "labLog";
+            this.labLog.Size = new System.Drawing.Size(90, 45);
+            this.labLog.TabIndex = 3;
+            this.labLog.Text = "歷程";
+            // 
+            // picResult
+            // 
+            this.picResult.Location = new System.Drawing.Point(409, 85);
+            this.picResult.Name = "picResult";
+            this.picResult.Size = new System.Drawing.Size(303, 357);
+            this.picResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picResult.TabIndex = 1;
+            this.picResult.TabStop = false;
+            // 
+            // frmGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.labLog);
+            this.Controls.Add(this.rtbLog);
+            this.Controls.Add(this.picResult);
+            this.Controls.Add(this.btnDraw);
+            this.Name = "frmGame";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmGame_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.PictureBox picResult;
+        private System.Windows.Forms.RichTextBox rtbLog;
+        private System.Windows.Forms.Label labLog;
+    }
+}
+
---
+++ WP_2020_01_HW02_A32000/frmGame.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WP_2020_01_HW02_A32000.Properties;
+
+namespace WP_2020_01_HW02_A32000
+{
+    public partial class frmGame : Form
+    {
+        List<Image> list = new List<Image>();
+
+        public frmGame()
+        {
+            InitializeComponent();
+        }
+
+        private void frmGame_Load(object sender, EventArgs e)
+        {
+            list.Add(Resources._01);
+            list.Add(Resources._02);
+            list.Add(Resources._03);
+            list.Add(Resources._04);
+            list.Add(Resources._05);
+            list.Add(Resources._06);
+            list.Add(Resources._07);
+            list.Add(Resources._08);
+            list.Add(Resources._09);
+            list.Add(Resources._10);
+            list.Add(Resources._11);
+            list.Add(Resources._12);
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            Poker p = new Poker();
+            List<int> poker = p.GetPoker(12);
+
+            string msg = "";
+            for (int i = 0; i < 1; i++)
+            {
+                msg += $"{poker[i] +1 } ";
+
+            }
+            rtbLog.Text += msg + "\n";
+            picResult.Image = list[poker[0]];
+            //int index = 3;//模擬隨機產生 一個值;
+            //picResult.Image = list[index];
+        }    
+    }
+}
